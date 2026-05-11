00822951e181e2fbc81034ef4975940f597e86de
Elisabed <elisabedisheshaberidze@gmail.com>
2026-05-11 23:29:52 +0400

    Started User class. WroteUsernam validation. Password pass

--- personal-expense-tracker/user.py
+++ personal-expense-tracker/user.py
@@ -1,7 +1,30 @@
+import re
 class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-    def validate_username(self):
-        pass
+        @property
+        def username(self):
+            return self._username
+        
+
+        @username.setter
+        def username(self, value):
+            if len(value.strip()) < 4 or len(value.strip()) > 20:
+                raise ValueError("Username must be between 4 and 20 characters.")
+            elif not all(char.isalnum() for char in value.strip()):
+                raise ValueError('Username must contain only letters and numbers.')
+        
+        @property
+        def password(self):
+            return self._password
+        
+        @password.setter
+        def password(self, value):
+            pass
+
+        
+
+
+    
