56f9fe9f32a5b9c282de2129982d06b235d76bdc
Elisabed <elisabedisheshaberidze@gmail.com>
2026-05-16 21:55:27 +0400

    Changed

--- personal-expense-tracker/database.py
+++ personal-expense-tracker/database.py
@@ -1,17 +1,28 @@
 import sqlite3
 
 def connect_db():
-    """Connects to the SQLite database and returns the connection object."""
-    return sqlite3.connect('expense_tracker.db')
-
-def cursor():
-    """Returns cursor object for executing SQL commands."""
-    return connect_db().cursor()
+    """Connects to the SQLite database and returns cursor object for executing SQL commands."""
+    conn = sqlite3.connect('expense_tracker.db')
+    return conn
 
 def create_user_table():
+    '''Creates the users table in the database if it doesn't already exist.'''
     with connect_db() as conn:
-        cursor().execute('''
+        conn.execute('''
             CREATE TABLE IF NOT EXISTS users(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username )
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL)
 ''')
+
+def insert_information(username, password):
+    '''Inseets a new user into the users table.'''
+    with connect_db() as conn:
+        conn.execute('''INSERT INTO users (username, password)
+                     VALUES(?, ?)''', (username, password))
+
+def delete_user(username):
+    '''Deletes a user from the user table based on the provided username.'''
+    with connect_db() as conn:
+        conn.execute('''DELETE FROM users WHERE username = ?''', (username,))
+
